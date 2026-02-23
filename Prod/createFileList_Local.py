d0e32a8b63468abf24f83d54987e12decab9f30d
Karim ElMorabit <karim.el.morabit@cern.ch>
2026-02-23 12:00:11 +0100

    changes for new skims

--- Prod/createFileList_Local.py
+++ Prod/createFileList_Local.py
@@ -1,9 +1,9 @@
 import os
 import glob
 
-output_file = "list_cff_Skim_2026_L1_v1_0_0_ZeroBias_2025G.py"
+output_file = "list_cff_Skim_2026_L1_v1_0_0_398683_PU7noQuad.py"
 
-input_wildcard = "/eos/cms/store/group/tsg/STEAM/L1Skims_ZB_L1_2026_v1_0_0/*/*.root"
+input_wildcard = "/eos/cms/store/group/tsg/STEAM/L1Skims_EZB_L1_2026_v1_0_0/398683_PU7_noQuad/*.root"
 
 def create_file_list(input_wildcard, output_file):
     all_files = glob.glob(input_wildcard)
